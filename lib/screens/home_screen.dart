bc8c6f17e8678375e347d98850ec5b61f9371e52
noelj <noeljabraham35@gmail.com>
2023-05-21 12:00:36 +0530

    after creating the box inside the homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,15 +26,60 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:
-            Text(_pageTitles[_selectedIndex]), // Update the title dynamically
+        title: Text(_pageTitles[_selectedIndex]),
         backgroundColor: Colors.purple,
-        automaticallyImplyLeading: false,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text(
-            _pageTitles[_selectedIndex]), // Update the body content dynamically
-      ),
+      body: _selectedIndex == 0
+          ? Column(
+              children: [
+                SizedBox(height: 16),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Stay strong and break free from addiction!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 8),
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.purple[200],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(_pageTitles[_selectedIndex]),
+                  ),
+                ),
+              ],
+            )
+          : Center(
+              child: Text(_pageTitles[_selectedIndex]),
+            ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,

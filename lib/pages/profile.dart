0a1a5f1d221de84c9b31e6a5c297fa241e2f3528
noelj <noeljabraham35@gmail.com>
2023-06-01 19:37:33 +0530

    after adding color theme

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -12,27 +13,35 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   bool locationAlert = false;
+  bool colorTheme = false;
 
   @override
   void initState() {
     super.initState();
-    // Load the location alert state when the widget initializes
-    loadLocationAlertState();
+    // Load the saved settings when the widget initializes
+    loadSettings();
   }
 
-  Future<void> loadLocationAlertState() async {
+  Future<void> loadSettings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool? alertState = prefs.getBool('locationAlert');
+    bool? themeState = prefs.getBool('colorTheme');
     if (alertState != null) {
       setState(() {
         locationAlert = alertState;
       });
     }
+    if (themeState != null) {
+      setState(() {
+        colorTheme = themeState;
+      });
+    }
   }
 
-  Future<void> saveLocationAlertState(bool value) async {
+  Future<void> saveSettings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('locationAlert', value);
+    await prefs.setBool('locationAlert', locationAlert);
+    await prefs.setBool('colorTheme', colorTheme);
   }
 
   void toggleLocationAlert(bool value) {
@@ -40,8 +49,8 @@ class _ProfileState extends State<Profile> {
       locationAlert = value;
     });
 
-    // Save the state when the switch value changes
-    saveLocationAlertState(value);
+    // Save the settings when the switch value changes
+    saveSettings();
 
     // Handle enabling/disabling the location_alert code here
     if (value) {
@@ -52,6 +61,26 @@ class _ProfileState extends State<Profile> {
       // Deactivate the location_alert code
       // Add your code here for disabling the location_alert functionality
       print('Location Alert disabled');
+    }
+  }
+
+  void toggleColorTheme(bool value) {
+    setState(() {
+      colorTheme = value;
+    });
+
+    // Save the settings when the switch value changes
+    saveSettings();
+
+    // Handle enabling/disabling the color theme code here
+    if (value) {
+      // Activate the color theme code
+      // Add your code here for enabling the color theme functionality
+      print('Color Theme enabled');
+    } else {
+      // Deactivate the color theme code
+      // Add your code here for disabling the color theme functionality
+      print('Color Theme disabled');
     }
   }
 
@@ -141,12 +170,55 @@ class _ProfileState extends State<Profile> {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            subtitle: Text(
+              'Alerts you before stepping in',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
             trailing: CupertinoSwitch(
               value: locationAlert,
               onChanged: toggleLocationAlert,
             ),
             onTap: () {
               toggleLocationAlert(!locationAlert);
+            },
+          ),
+          Divider(
+            color: Colors.grey.withOpacity(0.2),
+            thickness: 1,
+            indent: 16,
+            endIndent: 16,
+          ),
+          ListTile(
+            leading: Container(
+              padding: EdgeInsets.only(left: 4),
+              child: Icon(
+                Icons.color_lens,
+                color: Colors.deepPurple,
+              ),
+            ),
+            title: Text(
+              'Color Theme',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              'Change the color theme of the app',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+            trailing: CupertinoSwitch(
+              value: colorTheme,
+              onChanged: toggleColorTheme,
+            ),
+            onTap: () {
+              toggleColorTheme(!colorTheme);
             },
           ),
           Divider(
